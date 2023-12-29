db482cc31b878670b6f8d2a2710562ab54bb0a5b
Abraham Audu <ab6ril@gmail.com>
2023-12-29 18:23:08 +0100

    Wrote first C++ program

---
+++ FirstCppProgram/main.cpp
@@ -0,0 +1,15 @@
+// import iostream library
+#include <iostream>
+
+
+int main(){
+    // print `Abraham Audu` ten times
+    for (int i=0; i <= 10; i++){
+        std::cout << "Abraham Audu" << std::endl;
+    }
+    /*
+        This is a block comment
+        Just for the fun of it 
+    */
+    return 0;
+}
